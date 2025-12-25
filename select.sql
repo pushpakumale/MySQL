f85e0ec16eaacfe1248d39251a83249e23910194
Pushpak Umale <umalepushpak@gmail.com>
2025-12-26 00:09:59 +0530

    Add files via upload

---
+++ select.sql
@@ -0,0 +1,10 @@
+select * from employee_demographics;
+
+select first_name, last_name, birth_date
+from employee_demographics;
+
+#PEMDAS - Paranthesis Exponential Multiplication Division Substraction
+
+select count(distinct gender) as Number_of_Gender from employee_demographics; #Gives the total number of distinct gender in the table 
+
+
